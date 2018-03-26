ffe0bc9d10fca9da52f7b43247b683951ba02a2a
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-03-27 01:19:21 +0600

    Admin Login Page Design is done

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,20 @@
 <?php
 
-
+// Frontend Site Route............................................
 Route::get('/', 'HomeController@index');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Backend Site Route..............................................
+Route::get('/admin', 'AdminController@index');
